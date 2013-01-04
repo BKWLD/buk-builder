54be1ad9ee6c7c8c8fb14cde3445a4e20bdc851e
Dan Rogers <dan.rogers@bkwld.com>
2013-01-04 12:49:56 -0800

    Change compass default images dir to 'img'

--- config/compass.rb
+++ config/compass.rb
@@ -6,12 +6,12 @@ sass_dir                   = "sass"
 _public                    = "public/"
 css_dir                    = _public + "css"
 javascripts_dir            = _public + "js"
-images_dir                 = _public + "images"
+images_dir                 = _public + "img"
 
 http_path                  = "/"
 http_stylesheets_path      = http_path + "css"
 http_javascripts_path      = http_path + "js"
-http_images_path           = http_path + "images"
+http_images_path           = http_path + "img"
 http_generated_images_path = http_images_path
 
 # You can select your preferred output style here (can be overridden via the command line):
